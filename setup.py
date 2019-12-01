4c02489ece860d3ea95d100c618267ad87903350
141592653 <julien.rixte@laposte.net>
2019-12-01 19:57:26 +0100

    When it works it's better

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 import setuptools
+import os
+
+here = os.path.dirname(os.path.realpath(__file__))
+requirementPath = here + '/requirements.txt'
+install_requirements = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requirements = f.read().splitlines()
 
 
 with open('README.md') as f:
@@ -8,34 +16,25 @@ with open('README.md') as f:
 
 
 setuptools.setup(
-    name='webapp',
+    name='Granolar',
     version='1.0',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
     description='Example of Python web app with debian packaging (dh_virtualenv & systemd)',
     long_description=long_description,
-    author='Michal Horejsek',
-    author_email='horejsekmichal@gmail.com',
-    url='https://github.com/horejsek/python-webapp',
+    author='Ninon Devis, Cyril Lavrat, Emanouil Plitsis, Alice Rixte, Lydia Rodriguez de la Nava',
+    author_email='',
+    url='https://github.com/fouVReaux/Granolar',
 
     # All versions are fixed just for case. Once in while try to check for new versions.
-    install_requires=[
-        'flask==1.0.2',
-        'psycopg2==2.7.5',
-    ],
+    install_requires=install_requirements,
 
     # Do not use test_require or build_require, because then it's not installed and
     # can be used only by setup.py. We want to use it manually as well.
     # Actually it could be in file like dev-requirements.txt but it's good to have
     # all dependencies close each other.
-    extras_require={
-        'devel': [
-            'mypy==0.620',
-            'pylint==2.1.1',
-            'pytest==3.7.1',
-        ],
-    },
+    extras_require={"dev" : []},
 
     entry_points={
         'console_scripts': [
