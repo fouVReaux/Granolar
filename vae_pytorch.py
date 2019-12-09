4d970bd2de90fba03ffd7ced215ec4ca4a8955cb
Ninon Devis <ninon.devis@gmail.com>
2019-12-09 18:44:01 +0100

    refactor w/ good beta

--- vae_pytorch.py
+++ vae_pytorch.py
@@ -4,13 +4,11 @@
 from __future__ import print_function
 import argparse
 import os
-import torch
-from torch import optim
 
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
-from vae_model import VAE_Model
+from vae import VAE
 
 
 # get the arguments, if not on command line, the arguments are the default
@@ -28,97 +26,25 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 
 args = parser.parse_args()
 
-args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 
-torch.manual_seed(args.seed)
 
-# device is gpu if possible
-device = torch.device("cuda" if args.cuda else "cpu")
-
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
 # loading the training dataset
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+train_dataset = datasets.MNIST('../data', train=True, download=True, transform=transforms.ToTensor())
     
 # loading the test dataset
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
-
-# send the model to device
-model = VAE_Model().to(device)
-
-# set the optimizer
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = torch.nn.functional.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    return BCE + KLD
-
-
-def train(epoch):
-    model.train()
-    train_loss = 0
-    # for each batch
-    for batch_idx, (data, _) in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        # get the variables
-        recon_batch, mu, logvar = model(data)
-        # define the loss function
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        # affichage
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
-
-
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
-            data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            #affichage
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                      recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                         'results/reconstruction_' + str(epoch) + '.png', nrow=n)
-
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+test_dataset = datasets.MNIST('../data', train=False, transform=transforms.ToTensor())
 
 
 # main code
 if __name__ == "__main__":
     if not os.path.exists('results/'):
         os.makedirs('results')
-    for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        test(epoch)
-        with torch.no_grad():
-            sample = torch.randn(64, 20).to(device)
-            sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 1, 28, 28),
-                       'results/sample_' + str(epoch) + '.png')
+    vae = VAE(train_dataset, test_dataset, batch_size=args.batch_size, seed=args.seed, no_cuda=args.no_cuda)
+    for epoch in range(args.epochs):
+        vae.train()
+        vae.test()
+        sample = vae.create_sample()
+        save_image(sample.view(64, 1, 28, 28), 'results/sample_' + str(epoch) + '.png')
 
