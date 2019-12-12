c0dd6dcda392bb5bbbeaa81c7ae2239c3db38041
Ninon Devis <ninon.devis@gmail.com>
2019-12-12 18:58:04 +0100

    WIP Vae Model

--- src/vae_model.py
+++ src/vae_model.py
@@ -9,26 +9,49 @@ class VAE_Model(nn.Module):
     def __init__(self):
         super(VAE_Model, self).__init__()
 
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.encoder = nn.Sequential(
+            nn.Conv1d(512, 400, kernel_size=5, stride=1),
+            nn.ReLU(),
+            torch.nn.BatchNorm1d(400),
+            nn.Conv1d(400, 200, kernel_size=8, stride=2),
+            nn.ReLU(),
+            torch.nn.BatchNorm1d(200),
+            nn.Conv1d(200, 100, kernel_size=10, stride=4),
+            nn.ReLU(),
+            torch.nn.BatchNorm1d(100),
+            nn.Conv1d(100, 20, kernel_size=13, stride=4),
+            nn.ReLU()).cuda()
+        self.fc1 = nn.Linear(20, 1000)
+        self.fc2 = nn.Linear(1000, 10)
 
-    def encode(self, x):
-        h1 = functional.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        self.fc3 = nn.Linear(20, 1000)
+        self.fc4 = nn.Linear(1000, 10)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose1d(512, 400, kernel_size=13, stride=4),
+            nn.ReLU(),
+            torch.nn.BatchNorm1d(400),
+            nn.ConvTranspose1d(400, 200, kernel_size=10, stride=4),
+            nn.ReLU(),
+            torch.nn.BatchNorm1d(200),
+            nn.ConvTranspose1d(200, 100, kernel_size=8, stride=2),
+            nn.ReLU(),
+            torch.nn.BatchNorm1d(100),
+            nn.ConvTranspose1d(100, 20, kernel_size=5, stride=1),
+            nn.ReLU())
 
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
+        print(self.encoder)
+        print(self.decoder)
+
+    def encode(self, signal):
+        x = self.encoder(signal)
+        return self.mu(x), self.log_var(x)
 
     def decode(self, z):
         h3 = functional.relu(self.fc3(z))
         return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        mu, log_var = self.encode(x.view(-1, 512))
+        z = self.reparameterize(mu, log_var)
+        return self.decode(z), mu, log_var
+
