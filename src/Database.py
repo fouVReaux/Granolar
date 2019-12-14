31a1bf69faa8d91739404a9ae063c229e8b5449e
Ninon Devis <ninon.devis@gmail.com>
2019-12-14 20:47:30 +0100

    define database as torch Dataset

--- src/Database.py
+++ src/Database.py
@@ -8,6 +8,8 @@ import numpy as np
 import os
 
 # Audio
+from torch.utils.data import Dataset
+
 SAMPLING_RATE = 22050
 SLICE_DURATION = 0.08
 AUDIO_FORMAT = "npz"
@@ -16,65 +18,73 @@ DEFAULT_DATABASE_PATH = '../database/raw'
 DEFAULT_SLICES_PATH = '../database/slices'
 
 
-class DataBase:
-    def __init__(self):
-        self.sr = 0
-        self.slices_path = ""
-        self.database_path = ""
-        self.slices_duration = 0
-        self.type = ""
-
-    # Setter -------------------------------------------------------------------
-    def set_sr(self, sr):
-        """
-        set the sampling rate of the database
-        """
-        self.sr = sr
-
-    def set_type(self, type):
-        """
-        """
-        self.type = type
-        return type
-
-    def set_slices_path(self, slices_path):
-        """
-        set the slice path
-        """
+class DataBase(Dataset):
+    def __init__(self, audio_files_path=DEFAULT_DATABASE_PATH, sampling_rate=SAMPLING_RATE, slice_duration=SLICE_DURATION,
+                 transform=None, save_slices=False, slices_path=DEFAULT_SLICES_PATH, audio_format=AUDIO_FORMAT):
+        self.sr = sampling_rate
         self.slices_path = slices_path
+        self.database_path = audio_files_path
+        self.slices_duration = slice_duration
+        self.save_slices = save_slices
+        self.type = audio_format
+        self.slices = []
+        self.transform = transform
+        # generate the slices
+        only_files = [f for f in os.listdir(self.database_path) if os.path.isfile(os.path.join(self.database_path, f))]
+        for file in only_files:
+            self.slices += self.__slice_file__(file)
 
-    def set_database_path(self, database_path):
-        """
-        set the database path
-        """
-        self.database_path = database_path
+    # Override abstract methods from Dataset -----------------------------------
+    def __len__(self):
+        return len(self.slices)
 
-    def set_slices_duration(self, duration):
-        """
-        set the duration of a slice (in second)
-        """
-        self.slices_duration = duration
+    def __getitem__(self, index):
+        item = self.slices[index]
+        if self.transform is not None:
+            item = self.transform(item)
+        # return data as a tuple of data, metadata (null)
+        return item
+
+    # # Setter -------------------------------------------------------------------
+    # def set_sr(self, sr):
+    #     """
+    #     set the sampling rate of the database
+    #     """
+    #     self.sr = sr
+    #
+    # def set_type(self, type):
+    #     """
+    #     """
+    #     self.type = type
+    #     return type
+    #
+    # def set_slices_path(self, slices_path):
+    #     """
+    #     set the slice path
+    #     """
+    #     self.slices_path = slices_path
+    #
+    # def set_database_path(self, database_path):
+    #     """
+    #     set the database path
+    #     """
+    #     self.database_path = database_path
+    #
+    # def set_slices_duration(self, duration):
+    #     """
+    #     set the duration of a slice (in second)
+    #     """
+    #     self.slices_duration = duration
 
     # Slicer -------------------------------------------------------------------
-    def slice_database(self):
+    def __slice_file__(self, file_name):
         """
-        slice all files into the database directory
-        """
-        onlyfiles = [f for f in os.listdir(self.database_path) if os.path.isfile(os.path.join(self.database_path, f))]
-
-        for file in onlyfiles:
-            self.slice_file(file)
-        return
-
-    def slice_file(self, file_name):
-        """
-        slice a given file and save it in the slice directory
+        PRIVATE: slice a given file and save it in the slice directory. Should not be called directly
         """
         # create a directory for the slices
-        path = os.getcwd()
         path = self.slices_path + "/"
 
-        # get the samplig rate of the file_name
+        # get the sampling rate of the file_name
         sr = librosa.get_samplerate(self.database_path + "/" + file_name)
         print("\t> load of the file : ", file_name)
         # Set the frame parameters to be equivalent to the librosa defaults
@@ -91,17 +101,20 @@ class DataBase:
                                 mono=True)
         # slice and save the data into output file
         index = 0
+        slices = []
         for y in stream:
             # resample our lovely data
             yrs = librosa.resample(y, sr, self.sr)
-            # save data into a sweety file of duration [second]
-            if self.type == "wav":
-                librosa.output.write_wav(path + str(index) + "_" + file_name, yrs, self.sr, norm=False)
-            elif self.type == "npz":
-                np.savez_compressed(path + str(index) + "_" + file_name + '.npz', self.sr, yrs)
+            if self.save_slices:
+                # save data into a sweety file of duration [second]
+                if self.type == "wav":
+                    librosa.output.write_wav(path + str(index) + "_" + file_name, yrs, self.sr, norm=False)
+                elif self.type == "npz":
+                    np.savez_compressed(path + str(index) + "_" + file_name + '.npz', self.sr, yrs)
             index += 1
+            slices.append(yrs)
         print("\t\tNumber of slices :", index)
-        return index
+        return slices
 
 
 if __name__ == "__main__":
@@ -110,10 +123,4 @@ if __name__ == "__main__":
     # run python Database.py in order to slice all the wav file into subfile of
     # 5 seconds
     # ---------------------------------------------------------------------------
-    db = DataBase()
-    db.set_database_path(DEFAULT_DATABASE_PATH)
-    db.set_slices_path(DEFAULT_SLICES_PATH)
-    db.set_sr(SAMPLING_RATE)
-    db.set_slices_duration(SLICE_DURATION)
-    db.set_type(AUDIO_FORMAT)
-    db.slice_database()
+    db = DataBase(save_slices=True)
