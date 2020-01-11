ccf1e5ea5c7335ed127cc1d5a79887fb1e778b1d
Ninon Devis <ninon.devis@gmail.com>
2020-01-11 20:58:23 +0100

    fix dim sizes

--- src/vae_model.py
+++ src/vae_model.py
@@ -7,10 +7,10 @@
 # -*- coding: utf-8 -*-
 
 import torch
-from torch import nn, optim
-from torch.nn import functional
+from torch import nn
+from torch.nn import functional as F
 
-import math
+#import math
 
 class VAE_Model(nn.Module):
     def __init__(self, batch_size, channel, grain_size):
@@ -24,7 +24,8 @@ class VAE_Model(nn.Module):
         strides = [1, 2, 4, 4]
         paddings = [3, 4, 5, 7]
         self.encoder = nn.Sequential()
-        l_out = grain_size
+        self.input_pad = 16
+        l_out = self.grain_size
         for i, (c_in, c_out, kernel, stride, padding) in enumerate(zip(channels[:-1], channels[1:], kernel_sizes, strides, paddings)):
             # padding = math.ceil(kernel/2)
             self.encoder.add_module("enc_conv_"+str(i), nn.Conv1d(c_in, c_out, kernel_size=kernel, stride=stride, padding=padding))
@@ -32,15 +33,17 @@ class VAE_Model(nn.Module):
             self.encoder.add_module("enc_relu_"+str(i), nn.ReLU())
             l_out = (l_out + 2 * padding - (kernel - 1) - 1) // stride + 1
             print('l_encode', l_out)
+
+        self.l_enc = l_out
         # gaussian encoder
-        self.latent_size = l_out
-        self.encoder_fc = nn.Linear(batch_size, l_out)
+        self.latent_size = 16
+        self.encoder_fc = nn.Linear(self.l_enc, self.latent_size)
         # Always have same dimensions
-        self.enc_mu = nn.Linear(l_out, l_out)
-        self.enc_log_var = nn.Linear(l_out, l_out)
+        self.enc_mu = nn.Linear(self.latent_size, self.latent_size)
+        self.enc_log_var = nn.Linear(self.latent_size, self.latent_size)
 
         # gaussian decoder
-        self.decoder_fc = nn.Linear(l_out, batch_size)
+        self.decoder_fc = nn.Linear(self.latent_size, self.l_enc)
         # gaussian decoder layers
         channels = channels[::-1]
         kernel_sizes = kernel_sizes[::-1]
@@ -56,8 +59,8 @@ class VAE_Model(nn.Module):
             l_out = (l_out - 1) * stride - 2 * padding + kernel
             print('l decode', l_out)
         self.decoder.add_module("dec_tanh", nn.Tanh())
-        self.dec_mu = nn.Linear(batch_size, batch_size)
-        self.dec_log_var = nn.Linear(batch_size, batch_size)
+        self.dec_mu = nn.Linear(self.grain_size, self.grain_size)
+        self.dec_log_var = nn.Linear(self.grain_size, self.grain_size)
 
         print(self.encoder)
         print('size_encoder:', len(self.encoder))
@@ -66,17 +69,20 @@ class VAE_Model(nn.Module):
 
     def encode(self, data):
         x = self.encoder(data)
-        x = x.view(-1, self.batch_size * self.channel * 1)
-        fc_x = functional.relu(self.encoder_fc(x))
+
+        x = x.view(self.batch_size, self.l_enc)
+        fc_x = F.relu(self.encoder_fc(x))
         return self.enc_mu(fc_x), self.enc_log_var(fc_x)
 
     def decode(self, z):
+        print(z.shape)
         fc_z = self.decoder_fc(z)
-        x = functional.relu(fc_z)
+        x = F.relu(fc_z)
         data_recon = self.decoder(x.unsqueeze(1))
+
         mu_recon = self.dec_mu(data_recon)
         log_var_recon = self.dec_log_var(data_recon)
-        print('size mu_recon:', mu_recon.size, 'size log_var_recon', log_var_recon.size)
+
         return data_recon, mu_recon, log_var_recon
 
     def reparameterize(self, mu_z, log_var_z):
@@ -89,5 +95,6 @@ class VAE_Model(nn.Module):
         mu_z, log_var_z = self.encode(data)
         print('size mu:', mu_z.size(), 'size log_var:', log_var_z.size())
         z = self.reparameterize(mu_z, log_var_z)
-        data_recon, mu_recon, log_var_recon = self.decode(z.view(289, 1))
+        data_recon, mu_recon, log_var_recon = self.decode(z)
+
         return data_recon, mu_z, log_var_z, mu_recon, log_var_recon
